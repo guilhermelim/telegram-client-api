149cf539d4449bb7efa6ac3f9bb852f5487d4bf7
Guilherme Lima <guilhermem.lima@outlook.com>
2022-06-29 03:03:05 -0300

    add a simple function to get the username with the api.

--- telegram_client_api/main.py
+++ telegram_client_api/main.py
@@ -1,18 +1,44 @@
+# FastAPI imports
+from multiprocessing.connection import wait
 import uvicorn
 from fastapi import FastAPI
-from telegram_client_api.client import (connect, get_dialogs)
+
+# Telethon imports
+from decouple import config
+from telethon import TelegramClient
+
+# Read environment variables from .env file
+api_id = config('API_ID')
+api_hash = config('API_HASH')
+
+# Get username and return in JSON
+async def getUsername():
+    client = TelegramClient('anon', api_id, api_hash)
+    await client.start()
+    me = await client.get_me()
+    await client.disconnect()
+    return { "username": me.username }
+
+async def getUsername_2():
+    client = TelegramClient('anon', api_id, api_hash)
+    await client.connect()
+    if not client.is_user_authorized():
+        return 'Login fail, need to run init_session'
+    else:
+        me = await client.get_me()
+        await client.disconnect()
+        return { "username": me.username }
 
 
+# Define FastAPI object
 app = FastAPI()
+
+# Define API route
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
-
-@app.get("/dialogs")
-async def dialogs():
+    # return await getUsername()
     return await get_dialogs()
 
-
+# Run the API server
 def start():
-    connect()
     uvicorn.run("telegram_client_api.main:app", host="127.0.0.1", port=8000, reload=True, workers=2)
